9a7f26d3c7a87542ee17bd700e5d240b0b3d0025
JosephSC0121 <josephsilvacasas0121@gmail.com>
2024-07-15 19:35:00 -0500

    update: models, schemas and security

--- app/security/auth.py
+++ app/security/auth.py
@@ -25,9 +25,6 @@ async def get_current_user(token : Annotated[str, Depends(oauth2_scheme)]):
         payload = jwt.decode(token, SECRET_KEY, algorithms=[ALGORITHM])
         username: str = payload.get("sub")
         user_id: int = payload.get("id")
-        if username is None or user_id is None:
-            raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED,
-                                detail="Could not validate user.")
         return {"username": username, "id": user_id}
     except JWTError:
         raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED,
