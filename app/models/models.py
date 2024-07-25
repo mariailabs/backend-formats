1362672341cf8444c3b064fb167f6fe0f501a7bf
JosephSC0121 <josephsilvacasas0121@gmail.com>
2024-07-24 22:51:35 -0500

    feat: add taxi model

--- app/models/models.py
+++ app/models/models.py
@@ -3,9 +3,21 @@ from models.database import Base
 
 class User(Base):
     __tablename__ = 'users'
+
     id = Column(Integer, primary_key=True, autoincrement=True)
+    username = Column(String(50), unique=True)
     name = Column(String(70))
     email = Column(String(50))
-    username = Column(String(50), unique=True)
     hashed_password = Column(String(70))
+    
+class Taxi(Base):
+    __tablename__ = 'taxi'
+
+    id = Column(Integer, primary_key=True, autoincrement=True)
+    town = Column(String(50), unique=True)
+    kilometers = Column(String(50), unique=True)
+    time = Column(String(50), unique=True)
+    price = Column(String(50), unique=True)
+
+
 
