6ff51319ed9da4b8c421133adaad156888aa6d45
JosephSC0121 <josephsilvacasas0121@gmail.com>
2024-08-04 14:29:16 -0500

    feat: add taxi router

--- app/main.py
+++ app/main.py
@@ -1,9 +1,10 @@
 from fastapi import FastAPI
-from routers import auth_router, user_router 
+from routers import auth_router, user_router , taxi_router
 
 app = FastAPI()
 app.include_router(auth_router.router)
 app.include_router(user_router.router)
+app.include_router(taxi_router.router)
 
 
 
