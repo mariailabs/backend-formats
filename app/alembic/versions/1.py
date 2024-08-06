dad65c47e8d4dfe371ee24e0e3715769605ae2f8
JosephSC0121 <josephsilvacasas0121@gmail.com>
2024-08-06 17:00:43 -0500

    feat: add client mighration

--- app/alembic/versions/1.py
+++ app/alembic/versions/1.py
@@ -35,8 +35,36 @@ def upgrade() -> None:
         sa.Column('price', sa.String(length=50), nullable=True),
         sa.PrimaryKeyConstraint('id'),
     )
+    op.create_table(
+        'client',
+        sa.Column('id', sa.Integer(), autoincrement=True, nullable=False),
+        sa.Column('fecha_llamada', sa.String(length=10), nullable=True),
+        sa.Column('tipo_documento', sa.String(length=50), nullable=True),
+        sa.Column('numero_documento', sa.String(length=50), nullable=True),
+        sa.Column('nombres', sa.String(length=70), nullable=True),
+        sa.Column('apellido', sa.String(length=70), nullable=True),
+        sa.Column('fecha_nacimiento', sa.String(length=10), nullable=True),
+        sa.Column('edad_en_anos', sa.Integer(), nullable=True),
+        sa.Column('edad_en_meses', sa.Integer(), nullable=True),
+        sa.Column('curso_vida', sa.String(length=50), nullable=True),
+        sa.Column('regimen', sa.String(length=50), nullable=True),
+        sa.Column('tipo_afiliado', sa.String(length=50), nullable=True),
+        sa.Column('tipo_poblacion', sa.String(length=50), nullable=True),
+        sa.Column('codigo_municipio', sa.String(length=50), nullable=True),
+        sa.Column('nombre_municipio', sa.String(length=70), nullable=True),
+        sa.Column('zona', sa.String(length=50), nullable=True),
+        sa.Column('ips_atencion', sa.String(length=70), nullable=True),
+        sa.Column('numero_telefono_reportado', sa.String(length=50), nullable=True),
+        sa.Column('nombre_y_apellido_acudiente', sa.String(length=70), nullable=True),
+        sa.Column('parentesco', sa.String(length=50), nullable=True),
+        sa.Column('telefono_de_notificacion', sa.String(length=50), nullable=True),
+        sa.Column('motivo_llamada_fallida', sa.String(length=100), nullable=True),
+        sa.Column('observaciones', sa.String(length=255), nullable=True),
+        sa.PrimaryKeyConstraint('id')
+    )
 
 def downgrade() -> None:
     # Drop tables
     op.drop_table('users')
     op.drop_table('taxi')
+    op.drop_table('client')
