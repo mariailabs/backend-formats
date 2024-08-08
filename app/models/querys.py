71858725b48609ca0ec37ae42713b2c1286d6264
JosephSC0121 <josephsilvacasas0121@gmail.com>
2024-08-08 18:34:08 -0500

    feat: add create client

--- app/models/querys.py
+++ app/models/querys.py
@@ -1,8 +1,16 @@
 from sqlalchemy.orm import Session
 from . import models
+from schemas.schemas import ClientBase
 
-def get_exercises(db: Session):
+def get_tarifs(db: Session):
     return db.query(models.Taxi)
 
 def get_client(db: Session):
     return db.query(models.Client)
+
+def create_client(db: Session, client: ClientBase):
+    db_client = models.Client(**client.dict())
+    db.add(db_client)
+    db.commit()
+    db.refresh(db_client)
+    return db_client
