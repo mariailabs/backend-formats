ea995e282aeb5a1cfa64238f20ad2d40973e0aa5
JosephSC0121 <josephsilvacasas0121@gmail.com>
2024-08-10 22:04:47 -0500

    feat: add cors

--- app/main.py
+++ app/main.py
@@ -1,7 +1,21 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from routers import auth_router, user_router, taxi_router, client_router
 
 app = FastAPI()
+
+origins = [
+    "http://localhost:3000", 
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins, 
+    allow_credentials=True,  
+    allow_methods=["*"],  
+    allow_headers=["*"],  
+)
+
 app.include_router(auth_router.router)
 app.include_router(user_router.router)
 app.include_router(taxi_router.router)
